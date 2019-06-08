911cbbafd63d988de407ffa9eb55eb82386b02a3
Kinson <kinson0410@gmail.com>
2019-06-08 18:36:13 +0800

    Add new class to css

--- js/scripts.js
+++ js/scripts.js
@@ -17,11 +17,15 @@ var repository=[
  }
 ]
 
-  document.write ('<h2 class="pokedex_name">');
+
     for(var i=0;i<repository.length;i++){
-      document.write(repository[i].name + '('+'height:' + repository[i].height + ')' + '<br>');
+      //start a head tag
+      document.write('<h2 clsss="pokedex_name">' repository[i].name + '<span class="sub_text">('+'height:' + repository[i].height + ')</span>');
       if(repository[i].height>3){
-        document.write('<div class="big_pokemon">Wow, That is big!</div>')
-      }
+        //if there any with height greater than 3 print span
+        //and close the head tag
+        document.write('<span class="big_pokemon">Wow, That is big!</span></h2>')
+      }else{
+        //else just close the head tag
+        document.write('</h2>')
     }
-  document.write ('</h2>');
